3e5b31e8338259d2cf147cbb6efe7af8cd02bed2
Rafael Lancry <100874345+RafaelLancry@users.noreply.github.com>
2025-09-16 18:03:12 -0300

    Levels

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
