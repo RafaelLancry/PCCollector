4f7c227f4eacd31b8679941d52ae2795767d552b
Rafael Lancry <100874345+RafaelLancry@users.noreply.github.com>
2025-09-25 07:03:50 -0300

    PCCollectorFinal
    Everything working fine, time to start the TCC project

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,68 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+    void Awake()
+    {
+        // Conta todas as instâncias de GameSession (ativas e inativas)
+        var sessions = FindObjectsByType<GameSession>(
+            FindObjectsInactive.Include,
+            FindObjectsSortMode.None
+        );
+
+        if (sessions.Length > 1)
+        {
+            // Já existe outra; destrói esta e sai para não chamar DontDestroyOnLoad
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        if (playerLives > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
+    private void TakeLife()
+    {
+        playerLives -= 1;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
+    }
+
+    void ResetGameSession()
+    {
+        FindAnyObjectByType<ScenePersist>().ResetScenePersist();
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+}
